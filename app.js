1c42feb40a2f5997c52cb4c0b91062ffbc7fabc2
odeozome <78852587+Divine-Blessing@users.noreply.github.com>
2022-07-16 08:41:10 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+alert("Divine's JavaScript Calculator");
+//     let printThis = "Welcome to my website";
+//         console.log(printThis);
+// let radius = 10;
+// const pi = 3;
+// radius += 5;
+
+// let result = radius * pi;
+// console.log(radius)
+let x, y;
+x = parseFloat(parseInt(prompt("Enter first number")));
+y = parseFloat(parseInt(prompt("Enter second number")));
+let z = x + y;
+let a = x - y;
+let b = x / y;
+let c = x * y;
+alert("The sum is : "+ z);
+alert("The subtraction is : "+ a);
+alert("The quotient is : "+ b);
+alert("The product is : "+ c);
+
+// let theName = "Divine";
+// let bestFood = "Indomie";
+// var names = "joy";
+// const password = "whatevs";
+// let isMarried = true
+// let nameCap = names[0].toUpperCase();
+// let bee = names.slice[(0, 2)];
+
+// console.log(bestFood[2]);
+// console.log(theName);
+// console.log(names);
+// console.log(password.length);
+// console.log(isMarried);
+// console.log(nameCap);
+// console.log(bee);
